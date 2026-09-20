303f899409cdf9669783e28e59ca4c226c199a0a
AsfandYar <asfandyarinsights@gmail.com>
2026-09-20 16:20:39 +0500

    Initial Commit

---
+++ VariableDataypes.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = "AsfandYar";
+  int age;
+  age = 22;
+
+  double height = 5.4;
+  print('your name is : ${name} \nAge is ${age}');
+  print("Your Height Is ${height}");
+
+}
---
+++ list.dart
@@ -0,0 +1,5 @@
+void main (){
+ List <String> friends; //List Declaration
+ friends=['Asfand','Sarfaraz','Azeem','Arsalan']; //List Iniallization
+ print(friends[2]);
+}
